8c60febd84dc866e07dd9f9ae96c0394d136bcd5
jmgcheng <jmgcheng@users.noreply.github.com>
2023-09-17 13:33:59 +0800

    push model-serialization

--- core/backend/serializers.py
+++ core/backend/serializers.py
@@ -1,23 +1,30 @@
 from rest_framework import serializers
 from .models import Article
 
+# check 06. Model Serializer
+# usage was again made on shell
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = '__all__'
 
-# if your using Serializer, then you need to specify all the fields of our model explicitly
-# check 05. Data Serialization
-# usage was made on shell
-class ArticleSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=200)
-    description = serializers.CharField()
-    slug = serializers.SlugField(read_only=True)
-    published = serializers.DateTimeField(read_only=True)
 
-    def create(self, validated_data):
-        return Article.objects.create(**validated_data)
+# # if your using Serializer, then you need to specify all the fields of our model explicitly
+# # check 05. Data Serialization
+# # usage was made on shell
+# class ArticleSerializer(serializers.Serializer):
+#     title = serializers.CharField(max_length=200)
+#     description = serializers.CharField()
+#     slug = serializers.SlugField(read_only=True)
+#     published = serializers.DateTimeField(read_only=True)
+
+#     def create(self, validated_data):
+#         return Article.objects.create(**validated_data)
     
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.description = validated_data.get('description', instance.description)
-        instance.slug = validated_data.get('slug', instance.slug)
-        instance.published = validated_data.get('published', instance.published)
-        instance.save()
-        return instance
+#     def update(self, instance, validated_data):
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.slug = validated_data.get('slug', instance.slug)
+#         instance.published = validated_data.get('published', instance.published)
+#         instance.save()
+#         return instance
