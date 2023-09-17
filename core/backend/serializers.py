ec4b77ca638251da335d96921aaf18b6c8f68866
jmgcheng <jmgcheng@users.noreply.github.com>
2023-09-17 13:17:58 +0800

    push data-serialization

---
+++ core/backend/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import Article
+
+
+# if your using Serializer, then you need to specify all the fields of our model explicitly
+# check 05. Data Serialization
+# usage was made on shell
+class ArticleSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=200)
+    description = serializers.CharField()
+    slug = serializers.SlugField(read_only=True)
+    published = serializers.DateTimeField(read_only=True)
+
+    def create(self, validated_data):
+        return Article.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get('description', instance.description)
+        instance.slug = validated_data.get('slug', instance.slug)
+        instance.published = validated_data.get('published', instance.published)
+        instance.save()
+        return instance
