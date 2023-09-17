b1fbf919cb1bdabc4ac2ff81d7afda3bf2dbb4a7
jmgcheng <jmgcheng@users.noreply.github.com>
2023-09-17 16:15:28 +0800

    push 09-class-based-api-view

--- core/backend/urls.py
+++ core/backend/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import article_list, article_details
+# from .views import article_list, article_details
+from .views import ArticleList, ArticleDetails
 
 
 urlpatterns = [
     # path('', article_list),
-    path('articles/', article_list),
-    path('articles/<slug:slug>/', article_details),
+    # path('articles/', article_list),
+    # path('articles/<slug:slug>/', article_details),
+
+    path('articles/', ArticleList.as_view()),
+    path('articles/<slug:slug>/', ArticleDetails.as_view()),
 ]
--- core/backend/views.py
+++ core/backend/views.py
@@ -2,11 +2,63 @@ from django.shortcuts import render
 from .models import Article
 from .serializers import ArticleSerializer
 from rest_framework.parsers import JSONParser
-from django.http import JsonResponse, HttpResponse
+from django.http import JsonResponse, HttpResponse, Http404
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
+
+
+
+# check 09. Class Based API View
+# browsable API UI and POSTMAN same usage as before
+# 
+class ArticleList(APIView):
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ArticleSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# check 09. Class Based API View
+# browsable API UI and POSTMAN same usage as before
+# 
+class ArticleDetails(APIView):
+    def get_object(self, slug):
+        try:
+            return Article.objects.get(slug=slug)
+        except Article.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, slug):
+        article = self.get_object(slug)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+    
+    def put(self, request, slug):
+        article = self.get_object(slug)
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, slug):
+        article = self.get_object(slug)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+
+
 
 
 # check 08. API View Decorator
@@ -22,21 +74,21 @@ from rest_framework import status
 #           "title": "api view decorator",
 #           "description": "This is the description in postman"
 #       }
-@api_view(['GET', 'POST'])
-def article_list(request):
-    if request.method == "GET":
-        articles = Article.objects.all()
-        serializer = ArticleSerializer(articles, many=True)
+# @api_view(['GET', 'POST'])
+# def article_list(request):
+#     if request.method == "GET":
+#         articles = Article.objects.all()
+#         serializer = ArticleSerializer(articles, many=True)
 
-        return Response(serializer.data)
+#         return Response(serializer.data)
     
-    elif request.method == "POST":
-        serializer = ArticleSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
+#     elif request.method == "POST":
+#         serializer = ArticleSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
             
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 # check 08. API View Decorator
@@ -54,27 +106,27 @@ def article_list(request):
 #       }
 #   DELETE
 #       http://127.0.0.1:8000/articles/test-my-postman/
-@api_view(['GET', 'PUT', 'DELETE'])
-def article_details(request, slug):
-    try:
-        article = Article.objects.get(slug=slug)
-    except Article.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def article_details(request, slug):
+#     try:
+#         article = Article.objects.get(slug=slug)
+#     except Article.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
     
-    if request.method == "GET":
-        serializer = ArticleSerializer(article)
-        return Response(serializer.data)
+#     if request.method == "GET":
+#         serializer = ArticleSerializer(article)
+#         return Response(serializer.data)
 
-    elif request.method == "PUT":
-        serializer = ArticleSerializer(article, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     elif request.method == "PUT":
+#         serializer = ArticleSerializer(article, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    elif request.method == "DELETE":
-        article.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     elif request.method == "DELETE":
+#         article.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 
