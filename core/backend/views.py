c72921112a0dfe69620bd519c983c6ca5ba0ef34
jmgcheng <jmgcheng@users.noreply.github.com>
2023-09-17 16:45:30 +0800

    push 11-generic-class-based-view

--- core/backend/views.py
+++ core/backend/views.py
@@ -12,35 +12,55 @@ from rest_framework import mixins
 from rest_framework import generics
 
 
-# check 10. Mixins in Class Based View
+# check 11. Generic Class Based View
 # browsable API UI and POSTMAN same usage as before
-class ArticleList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class ArticleList(generics.ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-    
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
 
-
-# check 10. Mixins in Class Based View
+# check 11. Generic Class Based View
 # browsable API UI and POSTMAN same usage as before
-class ArticleDetails(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-
+class ArticleDetails(generics.RetrieveUpdateDestroyAPIView):
     lookup_field = 'slug'
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
+
+
+
+
+
+
+# check 10. Mixins in Class Based View
+# browsable API UI and POSTMAN same usage as before
+# class ArticleList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
     
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+
+
+# check 10. Mixins in Class Based View
+# browsable API UI and POSTMAN same usage as before
+# class ArticleDetails(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+
+#     lookup_field = 'slug'
+
+#     def get(self, request, *args, **kwargs):
+#         return self.retrieve(request, *args, **kwargs)
     
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+#     def put(self, request, *args, **kwargs):
+#         return self.update(request, *args, **kwargs)
+    
+#     def delete(self, request, *args, **kwargs):
+#         return self.destroy(request, *args, **kwargs)
 
 
 
