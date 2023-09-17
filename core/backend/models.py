eff13d845db9f60fa6b274bfa6bab9ea541872c3
jmgcheng <jmgcheng@users.noreply.github.com>
2023-09-17 12:57:26 +0800

    base setup

---
+++ core/backend/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    slug = models.SlugField(max_length=200)
+    published = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
